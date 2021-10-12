d65364c850e60388fba8e17ea708d23fa8c903c5
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-12 16:26:31 +0200

    riscritto js, riprovo a fare la funzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
-document.getElementsByClassName(".faBars").addEventListener ("click", function()) {
-  document.getElementsByClassName(".header-right")  
+const hamburger = document.getElementsByClassName("fa-bars");
+
+const listItems = document.querySelector(".header-right ul");
+
+function openMenu () {
+        
 }
+
+hamburger.addEventListener("click", openMenu);
