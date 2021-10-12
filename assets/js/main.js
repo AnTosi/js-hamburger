f293aadbb463535682a8af757be7dbd36176a043
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-12 18:18:47 +0200

    aggiunti commenti

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
+/* imposto le costanti che mi serviranno da pulsante e la selezione che mi servirà per applicare la proprietà active */
 const hamburger = document.querySelector(".fa-bars");
 
 const listItems = document.querySelector(".hamburger-menu");
 
 const cross = document.querySelector(".fa-times");
 
+/*  funzione di apertura menu e event listener con click */
 function openMenu() {
     listItems.classList.add("active");
 }
 
 hamburger.addEventListener("click", openMenu);
 
+/* funzione di chiusura menu con click */
 function closeMenu() {
     listItems.classList.remove("active");
 }
 
 cross.addEventListener("click", closeMenu);
+
