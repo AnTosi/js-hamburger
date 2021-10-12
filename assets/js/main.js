e2aa2e320c8ff87fd1c3fb13ba47b5eaa2516edb
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-12 16:28:42 +0200

    niente, non me lo prende

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const hamburger = document.getElementsByClassName("fa-bars");
 const listItems = document.querySelector(".header-right ul");
 
 function openMenu () {
-        
+    listItems.style.display ="block";
 }
 
 hamburger.addEventListener("click", openMenu);
